8a412b573941ab1eaca75f171bd80de4e313dc4a
Muhammad Mahmoud <87485763+skipcl4ss@users.noreply.github.com>
2024-08-28 15:08:17 +0300

    Tried changing groq but no luck
    tried experimenting with chathuggingface in test.ipynb but with no luck, also edited minor details in ui.py

--- ui.py
+++ ui.py
@@ -1,15 +1,15 @@
+from pprint import pprint
+import os
 import streamlit as st
 from pypdf import PdfReader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_chroma import Chroma
-from pprint import pprint
 from langchain_groq import ChatGroq
-import os
 
 # Read the API key from a file
-with open("C:\Programming\doc reader chatbot\groq.txt", "r") as file:
-    apiKey = file.read().strip()
+with open("C:\Programming\doc reader chatbot\groq.txt", "r") as apiKeyfile:
+    apiKey = apiKeyfile.read().strip()
 # Set the API key as an environment variable
 os.environ["GROQ_API_KEY"] = apiKey
 
@@ -26,15 +26,6 @@ def main():
 
     if file is not None:
         pdf = PdfReader(file)
-        st.header("debug")
-        st.text("debug")
-        st.markdown("debug")
-        st.write("debug")
-        # st.markdown(pdf)
-        # st.markdown(pdf.pages)
-        # st.markdown(len(pdf.pages))
-        # st.markdown(pdf.metadata)
-        # st.markdown(type(pdf))
         text = ""
         for page in pdf.pages:
             text += page.extract_text()
@@ -42,7 +33,7 @@ def main():
         db = processText(text)
 
         # query = st.text_input("Ask the AI a question about your file", "What is the premise of the story?")
-        query = st.text_input("Ask the AI a question about your file", "What is the premise of the story?")
+        query = st.text_input("Ask the AI a question about your file", placeholder = "What is the premise of the story?")
         confirm = st.button("Confirm")
         if confirm:
             cancel = st.button('Cancel')
@@ -56,13 +47,11 @@ def main():
                 pprint(results)
 
                 response = analyzeResults(query, results)
+                st.header("AI response")
                 st.markdown(response)
                 st.write(response)
 
 
-    # st.header("AI response")
-
-    # response = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut imperdiet est. Curabitur sollicitudin, mi porttitor auctor pharetra, tortor ipsum viverra dolor, eu dictum odio magna eu felis. Donec faucibus imperdiet tortor in tristique. Curabitur auctor sapien vel tempor tristique. Suspendisse convallis est ut hendrerit volutpat. Mauris eget elit vitae mauris finibus finibus. Phasellus sed dolor eget lorem tincidunt ullamcorper vitae id lacus. Nunc ullamcorper lorem ac tortor auctor dictum. Morbi sit amet eros aliquam, finibus quam et, hendrerit nisl. Donec erat augue, venenatis sit amet vulputate at, tincidunt sed sem. Pellentesque imperdiet nisi pharetra erat molestie, quis dapibus velit pharetra. Curabitur sollicitudin nunc sed cursus consectetur. Nulla ultricies molestie nisi et posuere.\n\nSed eleifend nisl at eleifend accumsan. Mauris purus odio, dictum sit amet arcu quis, imperdiet ultrices nisl. Etiam egestas vel sapien id porta. Praesent at accumsan justo, sit amet sollicitudin ipsum. Nullam scelerisque dignissim nisl id scelerisque. Mauris posuere est sapien, vitae imperdiet orci tincidunt vitae. Nullam in est et metus imperdiet ultrices. Nunc augue nibh, porttitor elementum libero vel, rutrum hendrerit ex. Suspendisse elementum condimentum turpis, vitae imperdiet neque molestie eu. Sed sed sapien efficitur, mattis nunc eu, porta diam. Nullam non fermentum enim. Nam et velit augue. Curabitur egestas mi vitae porta vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus dignissim sem quis augue pellentesque, in tincidunt magna porttitor."
 
     # st.markdown(f"{response}")
 
@@ -78,7 +67,7 @@ def processText(text):
     print(f"Split {len(text)} documents into {len(chunks)} chunks.")
 
     # Convert the chunks of text into embeddings to form a knowledge base
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    embeddings = HuggingFaceEmbeddings()
     db = Chroma.from_texts(chunks, embeddings)
 
     return db
@@ -124,7 +113,7 @@ def calculateChunkIDs(chunks):
             currentChunkIndex = 0
 
         # Calculate the chunk ID.
-        chunkID = f"{currentPageID}:{current_chunk_index}"
+        chunkID = f"{currentPageID}:{currentChunkIndex}"
         lastPageID = currentPageID
 
         # Add it to the page meta-data.
